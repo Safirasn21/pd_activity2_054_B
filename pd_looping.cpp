16b969cf861a7bc83647d268911f6a3ebba235d3
safirasn21 <safirasn021@gmai.com>
2023-03-22 17:55:33 +0700

    looping

--- pd_looping.cpp
+++ pd_looping.cpp
@@ -12,3 +12,60 @@ float Luassegitiga(float a, float t) {
 float Luaskingkaran(float r) {
 	return 3.14 * r * r;
 }
+
+int main() {
+
+	float panjang, lebar, tinggi, jejari, alas;
+	int pilihan; 
+
+
+	system("cls");
+
+	do
+	{
+		cout << "\n\n_________" << endl;
+		cout << "__M E N U__" << endl;
+		cout << "_______" << endl;
+
+		cout << "1. Luas Persegipanjang" << endl;
+		cout << "2. Luas Segitiga" << endl;
+		cout << "3. Luas Lingkaran" << endl;
+		cout << "4. Exit" << endl;
+
+		cout << "Masukan pilihan : ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukan Panjang : ";
+			cin >> panjang;
+			cout << "Masukan Lebar : ";
+			cin >> lebar;
+			cout << "Masukan Luas Persegipanjang = " << Luaspersegipanjang(panjang, lebar);
+			break;
+
+		case 2:
+			cout << "Masukan Alas : ";
+			cin >> alas;
+			cout << "Masukan Tinggi : ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << Luassegitiga(alas, tinggi);
+			break;
+
+		case 3:
+			cout << "Masukan jari-jari : ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = "; << LuasLingkaran(jejari);
+			break;
+
+		case 4: 
+
+			break;
+		default:
+			cout << "Pilihan anda salah!";
+			break;
+		}
+	} while (pilihan != 4);
+
+}
